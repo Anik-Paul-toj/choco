09894e24d42ed8f4283fa4ee8d1dba357c038c22
Anik-Paul-toj <trojanik003@gmail.com>
2025-02-09 20:03:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function showContent() {
+    document.querySelector(".magic-button").style.display = "none"; 
+    document.getElementById("content").style.display = "block"; 
+}
+
+function hugTeddy() {
+    alert("Hug me! 🥰");
+}
+
+function showMessage() {
+    alert("You are my favorite person! ❤️");
+}
+
+function showSurprise() {
+    alert("Surprise! A special day just for you! 🎁");
+}
